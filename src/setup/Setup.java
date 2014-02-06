536b229bc681100220be743dc65ab055c33d1a04
Matthew Janes <mjanes@gmail.com>
2014-02-05 21:04:46 -0800

    First changes to setup process, and addition of git ignore. Removing files that should not have been committed earlier.

--- src/setup/Setup.java
+++ src/setup/Setup.java
@@ -6,17 +6,35 @@ import java.util.Collection;
 import entity.BasePhysicalEntity;
 
 public class Setup {
+	
+	private static ArrayList<BasePhysicalEntity> entities;
 
 	public static Collection<BasePhysicalEntity> create() {
-		ArrayList<BasePhysicalEntity> entities = new ArrayList<BasePhysicalEntity>();
+		entities = new ArrayList<BasePhysicalEntity>();
+		basicOrbitCouple();
 		
+		//randomSet(10);
+		
+		return entities;
+	}
+	
+	private static void basicOrbitCouple() {
+
 		BasePhysicalEntity entityA = new BasePhysicalEntity(100, 100, 0, 500);
 		entityA.setDeltaX(1.0);
 		entities.add(entityA);
 				
-		entities.add(new BasePhysicalEntity(300, 300, 0, 4000));
-		
-		return entities;
+		entities.add(new BasePhysicalEntity(300, 300, 0, 8000));
+	}
+	
+	private static void randomSet(int n) {
+		BasePhysicalEntity newEntity;
+		for (int i = 0; i < n; ++i) {
+			newEntity = new BasePhysicalEntity(Math.random() * 600, Math.random() * 600, 0, Math.random() * 100);
+			newEntity.setDeltaX(Math.random() / 10);
+			newEntity.setDeltaY(Math.random() / 10);
+			entities.add(newEntity);
+		}
 	}
 	
 }
